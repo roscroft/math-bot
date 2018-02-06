34899d07faf07718cfe16458008796538e665090
Austin <adherrling@gmail.com>
2018-02-06 14:44:45 -0500

    Fixed repo directory level

--- schepbot/runschepbot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Running..."
-pkill -F /home/austin/Documents/schepbot/logs/pid.pid
-/usr/bin/python3.6 -u /home/austin/Documents/schepbot/schepbot.py -c >> /home/austin/Documents/schepbot/logs/log.log 2>&1 &
-echo $! > /home/austin/Documents/schepbot/logs/pid.pid
--- schepbot/runtelosbot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Running..."
-pkill -F /home/austin/Documents/schepbot/logs/pid2.pid
-/usr/bin/python3.6 -u /home/austin/Documents/schepbot/telosbot.py -b >> /home/austin/Documents/schepbot/logs/log2.log 2>&1 &
-echo $! > /home/austin/Documents/schepbot/logs/pid2.pid
