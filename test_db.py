b07c7042350df07fb6199169c8617324bac1c6cb
Austin <adherrling@gmail.com>
2018-09-03 16:37:32 -0400

    Refreshing submodule

---
+++ test_db.py
@@ -0,0 +1,35 @@
+import asyncio
+import asyncpg
+import datetime
+
+async def main():
+
+    conn = await asyncpg.connect('postgresql://austin:postgre@localhost/clan')
+
+    await conn.execute('''
+        CREATE TABLE account(
+            disc_id text PRIMARY KEY,
+            rsn text
+        )
+    ''')
+
+    await conn.execute('''
+        INSERT INTO account(disc_id, rsn) VALUES($1, $2)
+    ''', '215367025705484289', 'iMath')
+
+    row = await conn.fetchrow(
+        'SELECT * FROM account WHERE rsn = $1', 'iMath')
+
+    await conn.close()
+
+asyncio.get_event_loop().run_until_complete(main())
+
+# class Account(BASE):
+#     """Relates Discord IDs to RSNs"""
+#     __tablename__ = 'account'
+#     disc_id = Column(String(50), primary_key=True)
+#     rsn = Column(String(50), primary_key=True)
+#     last_cap_time = Column(DateTime)
+#     total_caps = Column(Integer)
+#     search_string = Column(String(50))
+#     satisfies = Column(Boolean)
